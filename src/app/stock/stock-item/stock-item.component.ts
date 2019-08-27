7b80d2313c480656d3ddf2f83f24bf99d60553a3
Collin <ococncol@gmail.com>
2019-08-27 06:31:16 -0400

    started binding

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -10,6 +10,7 @@ import { Stock } from '../../model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
+  public stockClasses;
 
   constructor() { }
 
@@ -20,6 +21,14 @@ export class StockItemComponent implements OnInit {
       85,
       80,
     );
+    const diff = (this.stock.price / this.stock.previousPrice - 1);
+    const largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      positive: this.stock.isPositiveChange(),
+      negative: !this.stock.isPositiveChange(),
+      'large-change': largeChange,
+      'small-change': !largeChange
+    }
   }
 
   toggleFavorite(event) {
